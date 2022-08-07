6b9ea2e5637d6dc610b7c2775a617bac537cc767
Daniel Szlaski <daniel.szlaski@genesys.com>
2022-08-07 09:58:29 +0200

    Main commit

--- README.md
+++ README.md
@@ -4,20 +4,21 @@
 
 <span align="center">
 
-# homebridge-web-fan
+# homebridge-solis5g-battery
 
-[![npm](https://img.shields.io/npm/v/homebridge-web-fan.svg)](https://www.npmjs.com/package/homebridge-web-fan) [![npm](https://img.shields.io/npm/dt/homebridge-web-fan.svg)](https://www.npmjs.com/package/homebridge-web-fan)
+[![npm](https://img.shields.io/npm/v/homebridge-solis5g-battery.svg)](https://www.npmjs.com/package/homebridge-solis5g-battery) [![npm](https://img.shields.io/npm/dt/homebridge-solis5g-battery.svg)](https://www.npmjs.com/package/homebridge-solis5g-battery)
 
 </span>
 
 ## Description
 
-This [homebridge](https://github.com/homebridge/homebridge) plugin exposes a web-based valve to Apple's [HomeKit](http://www.apple.com/ios/home/). Using simple HTTP requests, the plugin allows you to turn on/off the fan.
+This [homebridge](https://github.com/homebridge/homebridge) plugin read Battery configuration from your cloud Solis 5G account and exposes it as a Fan Accessory to Apple's [HomeKit](http://www.apple.com/ios/home/). 
+Battery % is displayed as rotationSpeed of the Fan.
 
 ## Installation
 
 1. Install [homebridge](https://github.com/homebridge/homebridge#installation)
-2. Install this plugin: `npm install -g homebridge-web-fan`
+2. Install this plugin: `npm install -g homebridge-solis5g-battery`
 3. Update your `config.json` file
 
 ## Configuration
@@ -25,9 +26,11 @@ This [homebridge](https://github.com/homebridge/homebridge) plugin exposes a web
 ```json
 "accessories": [
      {
-       "accessory": "WebFan",
-       "name": "Fan",
-       "apiroute": "http://myurl.com"
+       "accessory": "Solis5gBattery",
+       "name": "Battery",  
+       "solis_keyid": "",
+       "solis_secret": "",       
+       "solis_stationId": "<Your StationId from soliscloud>"     
      }
 ]
 ```
@@ -35,60 +38,22 @@ This [homebridge](https://github.com/homebridge/homebridge) plugin exposes a web
 ### Core
 | Key | Description | Default |
 | --- | --- | --- |
-| `accessory` | Must be `WebFan` | N/A |
+| `accessory` | Must be `Solis5gBattery` | N/A |
 | `name` | Name to appear in the Home app | N/A |
-| `apiroute` | Root URL of your device | N/A |
+| `solis_keyid` | Solis KeyId (obtain from Solis support) | N/A |
+| `solis_secret` | Solis Secret (obtain from Solis support) | N/A |
+| `solis_stationId` | Solis StationId (Your main StationId) | N/A |
 
 ### Optional fields
 | Key | Description | Default |
 | --- | --- | --- |
-| `rotationSpeed` | Whether to expose rotation speed as a characteristic | `false` |
-| `rotationDirection` | Whether to expose rotation direction as a characteristic | `false` |
+| `solis_url` | Main Solis URL where requests are send | `https://www.soliscloud.com:13333` |
 
 ### Additional options
 | Key | Description | Default |
 | --- | --- | --- |
 | `pollInterval` | Time (in seconds) between device polls | `300` |
-| `listener` | Whether to start a listener to get real-time changes from the device | `false` |
-| `timeout` | Time (in milliseconds) until the accessory will be marked as _Not Responding_ if it is unreachable | `3000` |
-| `port` | Port for your HTTP listener (if enabled) | `2000` |
-| `http_method` | HTTP method used to communicate with the device | `GET` |
-| `username` | Username if HTTP authentication is enabled | N/A |
-| `password` | Password if HTTP authentication is enabled | N/A |
 | `model` | Appears under the _Model_ field for the accessory | plugin |
 | `serial` | Appears under the _Serial_ field for the accessory | apiroute |
 | `manufacturer` | Appears under the _Manufacturer_ field for the accessory | author |
 | `firmware` | Appears under the _Firmware_ field for the accessory | version |
-
-## API Interfacing
-
-Your API should be able to:
-
-1. Return JSON information when it receives `/status`:
-```
-{
-    "currentState": INT_VALUE
-}
-```
-
-2. Set the state when it receives:
-```
-/setState?value=INT_VALUE
-```
-
-### Optional (if listener is enabled)
-
-1. Update `state` following a manual override by messaging the listen server:
-```
-/state?value=INT_VALUE
-```
-
-2. If `rotationSpeed` is enabled, update `rotationSpeed` following a manual override by messaging the listen server:
-```
-/rotationSpeed?value=INT_VALUE_0_TO_100
-```
-
-3. If `rotationDirection` is enabled, update `rotationDirection` following a manual override by messaging the listen server:
-```
-/rotationDirection?value=INT_VALUE_0_TO_1
-```
--- index.js
+++ index.js
@@ -1,116 +1,96 @@
 let Service, Characteristic
 const packageJson = require('./package.json')
 const request = require('request')
-const ip = require('ip')
-const http = require('http')
+
+const crypto = require('crypto');
+const moment = require('moment');
 
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service
   Characteristic = homebridge.hap.Characteristic
-  homebridge.registerAccessory('homebridge-web-fan', 'WebFan', WebFan)
+  homebridge.registerAccessory('homebridge-solis5g-battery', 'Solis5gBattery', Solis5G)
 }
 
-function WebFan (log, config) {
+function Solis5G (log, config) {
   this.log = log
 
-  this.name = config.name
-  this.apiroute = config.apiroute
+  this.SOLIS_AUTH = {
+    keyId: config.solis_keyid,
+    secret: config.solis_secret,
+    url: config.solis_url || 'https://www.soliscloud.com:13333'
+  }
+
+  this.name = config.name  
   this.pollInterval = config.pollInterval || 300
-
-  this.listener = config.listener || false
-  this.port = config.port || 2000
-  this.requestArray = ['state', 'rotationSpeed', 'rotationDirection']
-
-  this.rotationSpeed = config.rotationSpeed || false
-  this.rotationDirection = config.rotationDirection || false
-
+  this.solisStationId = config.solis_stationId;
+  
   this.manufacturer = config.manufacturer || packageJson.author
   this.serial = config.serial || this.apiroute
   this.model = config.model || packageJson.name
   this.firmware = config.firmware || packageJson.version
 
-  this.username = config.username || null
-  this.password = config.password || null
-  this.timeout = config.timeout || 3000
-  this.http_method = config.http_method || 'GET'
-
-  if (this.username != null && this.password != null) {
-    this.auth = {
-      user: this.username,
-      pass: this.password
-    }
-  }
-
-  if (this.listener) {
-    this.server = http.createServer(function (request, response) {
-      const baseURL = 'http://' + request.headers.host + '/'
-      const url = new URL(request.url, baseURL)
-      if (this.requestArray.includes(url.pathname.substr(1))) {
-        try {
-          this.log.debug('Handling request')
-          response.end('Handling request')
-          this._httpHandler(url.pathname.substr(1), url.searchParams.get('value'))
-        } catch (e) {
-          this.log.warn('Error parsing request: %s', e.message)
-        }
-      } else {
-        this.log.warn('Invalid request: %s', request.url)
-        response.end('Invalid request')
-      }
-    }.bind(this))
-
-    this.server.listen(this.port, function () {
-      this.log('Listen server: http://%s:%s', ip.address(), this.port)
-    }.bind(this))
-  }
-
   this.service = new Service.Fan(this.name)
 }
 
-WebFan.prototype = {
+Solis5G.prototype = {
 
   identify: function (callback) {
     this.log('Identify requested!')
     callback()
   },
 
-  _httpRequest: function (url, body, method, callback) {
+  _httpRequest: function (url, body, method, headers, callback) {            
     request({
       url: url,
       body: body,
-      method: this.http_method,
-      timeout: this.timeout,
-      rejectUnauthorized: false,
-      auth: this.auth
+      method: method,
+      headers: headers
     },
     function (error, response, body) {
       callback(error, response, body)
     })
   },
 
-  _getStatus: function (callback) {
-    const url = this.apiroute + '/status'
-    this.log.debug('Getting status: %s', url)
 
-    this._httpRequest(url, '', 'GET', function (error, response, responseBody) {
+  _getStatus: function (callback) {
+    this.log.debug('>> solis -> getData...');
+    const resource = '/v1/api/stationDetailList';
+    const body = `{"id":"${this.solisStationId}"}`;
+    let md = crypto.createHash("md5").update(body).digest();        
+    const contentMd5 = Buffer.from(md).toString('base64')
+    
+    let date = moment().toDate().toUTCString();        
+    let payload = `POST\n${contentMd5}\napplication/json\n${date}\n${resource}`;
+    let options = {
+      secret: this.SOLIS_AUTH.secret,
+      message: payload
+    }        
+    let sign = hmacSha1(options);
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `API ${this.SOLIS_AUTH.keyId}:${sign}`,
+      'Date': date,            
+      'Content-MD5': contentMd5
+    };
+
+    
+    
+    this._httpRequest(this.SOLIS_AUTH.url + resource, body, 'POST', headers, function (error, response, responseBody) {
       if (error) {
         this.log.warn('Error getting status: %s', error.message)
         this.service.getCharacteristic(Characteristic.On).updateValue(new Error('Polling failed'))
         callback(error)
       } else {
-        this.log.debug('Device response: %s', responseBody)
+        this.log.debug('Device response: %s', responseBody);        
         try {
-          const json = JSON.parse(responseBody)
-          this.service.getCharacteristic(Characteristic.On).updateValue(json.currentState)
-          this.log.debug('Updated state to: %s', json.currentState)
-          if (this.rotationSpeed) {
-            this.service.getCharacteristic(Characteristic.RotationSpeed).updateValue(json.rotationSpeed)
-            this.log.debug('Updated rotationSpeed to: %s', json.rotationSpeed)
-          }
-          if (this.rotationDirection) {
-            this.service.getCharacteristic(Characteristic.RotationDirection).updateValue(json.rotationDirection)
-            this.log.debug('Updated rotationDirection to: %s', json.rotationDirection)
-          }
+          const json = JSON.parse(responseBody)           
+          this.service.getCharacteristic(Characteristic.On).updateValue(1)
+          this.log.debug('Updated state to: %s', 1)
+          
+          this.service.getCharacteristic(Characteristic.RotationSpeed).updateValue(json.data.records[0].batteryPercent)
+          this.log.debug('Updated rotationSpeed to: %s', json.data.records[0].batteryPercent)
+          
           callback()
         } catch (e) {
           this.log.warn('Error parsing status: %s', e.message)
@@ -119,74 +99,6 @@ WebFan.prototype = {
     }.bind(this))
   },
 
-  _httpHandler: function (characteristic, value) {
-    switch (characteristic) {
-      case 'state': {
-        this.service.getCharacteristic(Characteristic.On).updateValue(value)
-        this.log('Updated %s to: %s', characteristic, value)
-        break
-      }
-      case 'rotationSpeed': {
-        this.service.getCharacteristic(Characteristic.RotationSpeed).updateValue(value)
-        this.log('Updated %s to: %s', characteristic, value)
-        break
-      }
-      case 'rotationDirection': {
-        this.service.getCharacteristic(Characteristic.RotationDirection).updateValue(value)
-        this.log('Updated %s to: %s', characteristic, value)
-        break
-      }
-      default: {
-        this.log.warn('Unknown characteristic "%s" with value "%s"', characteristic, value)
-      }
-    }
-  },
-
-  setOn: function (value, callback) {
-    value = value ? 1 : 0
-    const url = this.apiroute + '/setState?value=' + value
-    this.log.debug('Setting state: %s', url)
-
-    this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
-      if (error) {
-        this.log.warn('Error setting state: %s', error.message)
-        callback(error)
-      } else {
-        this.log('Set state to %s', value)
-        callback()
-      }
-    }.bind(this))
-  },
-
-  setRotationSpeed: function (value, callback) {
-    const url = this.apiroute + '/setRotationSpeed?value=' + value
-    this.log.debug('Setting rotationSpeed: %s', url)
-
-    this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
-      if (error) {
-        this.log.warn('Error setting rotationSpeed: %s', error.message)
-        callback(error)
-      } else {
-        this.log('Set rotationSpeed to %s', value)
-        callback()
-      }
-    }.bind(this))
-  },
-
-  setRotationDirection: function (value, callback) {
-    const url = this.apiroute + '/setRotationDirection?value=' + value
-    this.log.debug('Setting rotationDirection: %s', url)
-
-    this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
-      if (error) {
-        this.log.warn('Error setting rotationDirection: %s', error.message)
-        callback(error)
-      } else {
-        this.log('Set rotationDirection to %s', value)
-        callback()
-      }
-    }.bind(this))
-  },
 
   getServices: function () {
     this.informationService = new Service.AccessoryInformation()
@@ -195,22 +107,6 @@ WebFan.prototype = {
       .setCharacteristic(Characteristic.Model, this.model)
       .setCharacteristic(Characteristic.SerialNumber, this.serial)
       .setCharacteristic(Characteristic.FirmwareRevision, this.firmware)
-
-    this.service
-      .getCharacteristic(Characteristic.On)
-      .on('set', this.setOn.bind(this))
-
-    if (this.rotationSpeed) {
-      this.service
-        .getCharacteristic(Characteristic.RotationSpeed)
-        .on('set', this.setRotationSpeed.bind(this))
-    }
-
-    if (this.rotationDirection) {
-      this.service
-        .getCharacteristic(Characteristic.RotationDirection)
-        .on('set', this.setRotationDirection.bind(this))
-    }
 
     this._getStatus(function () {})
 
@@ -221,4 +117,8 @@ WebFan.prototype = {
     return [this.informationService, this.service]
   }
 
+}
+
+function hmacSha1 (options) {
+  return crypto.createHmac('sha1', options.secret).update(options.message).digest('base64')
 }
